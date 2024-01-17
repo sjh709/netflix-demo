1c7a0c9bfe781a42715a53a0326374d9ba1146bb
Jihee <sjh709@naver.com>
2024-01-17 17:45:22 +0900

    Route 구성하기 (App.js)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundpage/NotFoundPage';
 
+// 홈페이지 '/'
+// 전체 영화 보여주는 페이지 -> 서치 '/movies?q='
+// 영화 디테일 페이지 '/movies/:id'
+// 추천 영화 '/movies/:id/recommandation'
+// 리뷰 '/movies/:id/reviews'
 function App() {
   return (
-    <div className='App'>
-      <AppLayout />
-    </div>
+    <Routes>
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path='movies'>
+          <Route index element={<MoviePage />} />
+          <Route path=':id' element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
