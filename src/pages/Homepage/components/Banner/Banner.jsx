c6f27ee62141190edca39b02fc5fec984cd0d5d3
Jihee <sjh709@naver.com>
2024-01-18 19:22:35 +0900

    배너: 디자인하기

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
+import Alert from 'react-bootstrap/Alert';
+import './Banner.style.css';
 
 const Banner = () => {
-  const { data } = usePopularMoviesQuery();
-  //   console.log('data', data);
-  return <div>Banner</div>;
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+  //   console.log('data', data, isLoading, isError, error);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <Alert variant='danger'>{error.message}</Alert>;
+  }
+  return (
+    <div
+      style={{
+        backgroundImage:
+          'url(' +
+          `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${data.results[0].poster_path}` +
+          ')',
+      }}
+      className='banner'
+    >
+      <div className='text-white banner-text-area'>
+        <h1>{data.results[0].title}</h1>
+        <p>{data.results[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
