cdab402955a3521faf483667b99000812aab60d9
Jihee <sjh709@naver.com>
2024-01-19 16:02:15 +0900

    코드 구조 리팩토링

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
 import { Alert } from 'bootstrap';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import MovieCard from '../MovieCard/MovieCard';
-import './PopularMovieSlide.style.css';
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 6,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
+import MovieSlider from '../../../../common/MovieSlider/MovieSlider';
+import { responsive } from '../../../../constants/responsive';
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -31,18 +14,11 @@ const PopularMovieSlide = () => {
   }
   return (
     <div>
-      <h1 className='popular-title'>Popular Movies</h1>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass='movie-slider p-1'
-        containerClass='carousel-container'
+      <MovieSlider
+        title='Popular Movies'
+        movies={data.results}
         responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
+      />
     </div>
   );
 };
